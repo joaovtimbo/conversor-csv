b24f9f4a5c85be8630ea791eb3128c25fccfaf9f
João Victor <joaov.timbo@gmail.com>
2023-10-20 11:56:25 -0300

    while loop adicionado

--- csv-to-excel.py
+++ csv-to-excel.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
-print("Insira o diretório do arquivo: ")
+pd.set_option('display.max_colwidth', None)
 
-arquivo_csv = input()
-read_file = pd.read_csv(arquivo_csv)
-arquivo_csv = arquivo_csv.replace("csv","xlsx")
-read_file.to_excel(arquivo_csv, index=None, header=True)
+numero = 1
+while numero <= 6:
+    numero += 1
 
-print("O arquivo foi convertido com sucesso!")
+    print("Insira o diretório do arquivo: ")
+
+    arquivo_csv = input()
+    read_file = pd.read_csv(arquivo_csv, encoding="ISO-8859-1")
+
+    arquivo_csv = arquivo_csv.replace("csv","xlsx")
+    read_file.to_excel(arquivo_csv, sheet_name='planilha', na_rep='NaN', float_format=None, columns=None, header=True, index=False, index_label=None, startrow=0, startcol=0, engine=None, merge_cells=True, inf_rep='inf', freeze_panes=None, storage_options=None, engine_kwargs=None)
+   
+    print("O arquivo foi convertido com sucesso!\n")
